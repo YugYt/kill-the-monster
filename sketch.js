8e244d6383d7e3ced91885d6189c1001970570fb
YugYt <72304225+YugYt@users.noreply.github.com>
2021-02-11 19:33:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var hero
+
+function preload() {
+//preload the images here
+
+}
+
+function setup() {
+  createCanvas(400, 400);
+hero=createSprite(200, 200, 50, 20)
+
+}
+
+function draw() {
+  background(0);
+  drawSprites();
+
+
+}
+
